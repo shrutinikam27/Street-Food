00e17ce5d3b80beef240e29e765181aecdb91c11
shruti <nikamshruti27@gmail.com>
2025-07-27 12:29:28 +0530

    changed

--- street-food/src/App.jsx
+++ street-food/src/App.jsx
@@ -4,7 +4,8 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Homepage from './components/pages/homepage.jsx'
-
+import FindSuppliers from './components/pages/FindSupplier.jsx';
+import BecomeSupplier from './components/pages/BecomeSupplier.jsx';
 function App() {
   const [count, setCount] = useState(0)
 
@@ -12,6 +13,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/find-suppliers" element={<FindSuppliers />} />
+        <Route path="/become-supplier" element={<BecomeSupplier />} />
       </Routes>
     </BrowserRouter>
   )
